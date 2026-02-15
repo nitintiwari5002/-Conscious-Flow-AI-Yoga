d978a31ce27454ba51cba1514d3212ebc8cafe09
nitintiwari5002 <nitin.tiwari.5002@gmail.com>
2026-02-15 21:18:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
 Home = st.Page("pages/Home.py", title = "Home")
-Info = st.Page("pages/Info.py", title = "Info")
+Info = st.Page("pages/info.py", title = "Info")
 About = st.Page("pages/About.py", title = "About")
 Diet_Plan = st.Page("pages/Diet.py", title = "Diet Plan")
 pg = st.navigation([Home , Info, About, Diet_Plan], position = "top")
+
 
 pg.run()

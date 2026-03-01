370c37b6252cce5fc0db67a2b42386805a2cc92e
nitintiwari5002 <nitin.tiwari.5002@gmail.com>
2026-03-01 11:04:00 +0530

    Update yoga_recommendation.py

--- pages/yoga_recommendation.py
+++ pages/yoga_recommendation.py
@@ -1,5 +1,5 @@
 import streamlit as st
 from utils.ollama_client import pose_predictor
 
-a = st.textbox("Enter your problems to find the desired yoga pose...")
+a = st.text_input("Enter your problems to find the desired yoga pose...")
 pose_predictor(a)
